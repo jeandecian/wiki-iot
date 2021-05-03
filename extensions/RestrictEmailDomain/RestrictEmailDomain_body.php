c1a315abceebf11b4b618c64f86f1400ef34afdb
Jean Decian <jeandecian@hotmail.fr>
2021-05-03 00:34:57 -0400

    Restrict Email Domain allows subdomains

--- extensions/RestrictEmailDomain/RestrictEmailDomain_body.php
+++ extensions/RestrictEmailDomain/RestrictEmailDomain_body.php
@@ -6,20 +6,31 @@
  * @author Soma Deáki <sdeaki@gmail.com>
  */
 
-class RestrictEmailDomainPreAuthenticationProvider extends MediaWiki\Auth\AbstractPreAuthenticationProvider {
-	
-	public function testForAccountCreation($user, $creator, array $reqs) {
+class RestrictEmailDomainPreAuthenticationProvider extends MediaWiki\Auth\AbstractPreAuthenticationProvider
+{
+
+	public function testForAccountCreation($user, $creator, array $reqs)
+	{
 		global $wgEmailDomain;
-		
+
 		if ($wgEmailDomain) {
 			list($name, $host) = explode("@", $user->getEmail());
-			
-			if ((is_array($wgEmailDomain) && in_array($host, $wgEmailDomain)) || $host == $wgEmailDomain) {
-				return StatusValue::newGood();
+
+			if (is_array($wgEmailDomain)) {
+				foreach ($wgEmailDomain as $domain) {
+					$domain = trim($domain);
+					$subdomainPos = strpos($host, $domain);
+
+					if ($host === $domain) {
+						return StatusValue::newGood();
+					} elseif ($subdomainPos !== false and $host[$subdomainPos - 1] === '.') {
+						return StatusValue::newGood();
+					}
+				}
 			}
 		}
-		
-		$msg = wfMessage('restrictemaildomain-error')-> plain();
-		return StatusValue::newFatal( $msg );
+
+		$msg = wfMessage('restrictemaildomain-error')->plain();
+		return StatusValue::newFatal($msg);
 	}
 }
