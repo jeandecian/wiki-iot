6b785fa3d76eed668030793887059c05b50c6315
Jean Decian <jeandecian@hotmail.fr>
2021-05-13 16:41:41 -0400

    Customize NewestPages item list

--- extensions/NewestPages/SpecialNewestPages.php
+++ extensions/NewestPages/SpecialNewestPages.php
@@ -13,37 +13,40 @@
 
 use MediaWiki\MediaWikiServices;
 
-class SpecialNewestPages extends IncludableSpecialPage {
+class SpecialNewestPages extends IncludableSpecialPage
+{
 
 	private $limit = null;
 	private $namespace = null;
 	private $redirects = null;
 
-	public function __construct() {
-		parent::__construct( 'NewestPages', '', true, false, 'default', true );
+	public function __construct()
+	{
+		parent::__construct('NewestPages', '', true, false, 'default', true);
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	public function execute( $par ) {
+	public function execute($par)
+	{
 		$language = MediaWikiServices::getInstance()->getContentLanguage();
 
 		$out = $this->getOutput();
 		$request = $this->getRequest();
 
 		# Decipher input passed to the page
-		$this->decipherParams( $par );
-		$this->setOptions( $request );
+		$this->decipherParams($par);
+		$this->setOptions($request);
 
 		// Load style for class="allpagesredirect"
-		$out->addModuleStyles( 'mediawiki.special' );
+		$out->addModuleStyles('mediawiki.special');
 
-		$dbr = wfGetDB( DB_REPLICA );
+		$dbr = wfGetDB(DB_REPLICA);
 
 		$conds = [];
 		$conds[] = $this->getNsFragment();
-		if ( !$this->redirects ) {
+		if (!$this->redirects) {
 			$conds['page_is_redirect'] = 0;
 		}
 		$res = $dbr->select(
@@ -61,172 +64,190 @@ class SpecialNewestPages extends IncludableSpecialPage {
 				'OFFSET' => '0',
 			]
 		);
-		$count = $dbr->numRows( $res );
+		$count = $dbr->numRows($res);
 
 		# Don't show the navigation if we're including the page
-		if ( !$this->including() ) {
+		if (!$this->including()) {
 			$this->setHeaders();
-			$limit = $this->getLanguage()->formatNum( $this->limit );
-			if ( $this->namespace > 0 ) {
-				$out->addWikiMsg( 'newestpages-ns-header', $limit,
-					$language->getFormattedNsText( $this->namespace ) );
+			$limit = $this->getLanguage()->formatNum($this->limit);
+			if ($this->namespace > 0) {
+				$out->addWikiMsg(
+					'newestpages-ns-header',
+					$limit,
+					$language->getFormattedNsText($this->namespace)
+				);
 			} else {
-				$out->addWikiMsg( 'newestpages-header', $limit );
+				$out->addWikiMsg('newestpages-header', $limit);
 			}
 			$this->makeNamespaceForm();
-			$out->addHTML( '<p>' . $this->makeLimitLinks() );
-			$out->addHTML( '<br />' . $this->makeRedirectToggle() . '</p>' );
+			$out->addHTML('<p>' . $this->makeLimitLinks());
+			$out->addHTML('<br />' . $this->makeRedirectToggle() . '</p>');
 		}
 
-		if ( $count > 0 ) {
+		if ($count > 0) {
 			# Make list
-			if ( !$this->including() ) {
-				$out->addWikiMsg( 'newestpages-showing', $this->getLanguage()->formatNum( $count ) );
+			if (!$this->including()) {
+				$out->addWikiMsg('newestpages-showing', $this->getLanguage()->formatNum($count));
 			}
-			$out->addHTML( '<ol>' );
-			foreach ( $res as $row ) {
-				$out->addHTML( $this->makeListItem( $row ) );
+			$out->addHTML('<div id="newestpages-main">');
+			foreach ($res as $row) {
+				$out->addHTML($this->makeListItem($row));
 			}
-			$out->addHTML( '</ol>' );
+			$out->addHTML('</div>');
 		} else {
-			$out->addWikiMsg( 'newestpages-none' );
+			$out->addWikiMsg('newestpages-none');
 		}
 	}
 
 	/**
 	 * @param WebRequest $req
 	 */
-	private function setOptions( WebRequest $req ) {
-		$newestPagesLimit = $this->getConfig()->get( 'NewestPagesLimit' );
-		if ( !isset( $this->limit ) ) {
-			$this->limit = $this->sanitiseLimit( $req->getInt( 'limit', $newestPagesLimit ) );
+	private function setOptions(WebRequest $req)
+	{
+		$newestPagesLimit = $this->getConfig()->get('NewestPagesLimit');
+		if (!isset($this->limit)) {
+			$this->limit = $this->sanitiseLimit($req->getInt('limit', $newestPagesLimit));
 		}
-		if ( !isset( $this->namespace ) ) {
-			$this->namespace = $this->extractNamespace( $req->getVal( 'namespace', -1 ) );
+		if (!isset($this->namespace)) {
+			$this->namespace = $this->extractNamespace($req->getVal('namespace', -1));
 		}
-		if ( !isset( $this->redirects ) ) {
-			$this->redirects = (bool)$req->getInt( 'redirects', 1 );
+		if (!isset($this->redirects)) {
+			$this->redirects = (bool)$req->getInt('redirects', 1);
 		}
 	}
 
-	private function sanitiseLimit( $limit ) {
-		return min( (int)$limit, 5000 );
+	private function sanitiseLimit($limit)
+	{
+		return min((int)$limit, 5000);
 	}
 
-	private function decipherParams( $par ) {
-		if ( $par ) {
-			$bits = explode( '/', $par );
-			foreach ( $bits as $bit ) {
-				if ( is_numeric( $bit ) ) {
-					$this->limit = $this->sanitiseLimit( $bit );
+	private function decipherParams($par)
+	{
+		if ($par) {
+			$bits = explode('/', $par);
+			foreach ($bits as $bit) {
+				if (is_numeric($bit)) {
+					$this->limit = $this->sanitiseLimit($bit);
 				} else {
-					$this->namespace = $this->extractNamespace( $bit );
+					$this->namespace = $this->extractNamespace($bit);
 				}
 			}
 		}
 	}
 
-	private function extractNamespace( $namespace ) {
+	private function extractNamespace($namespace)
+	{
 		$language = MediaWikiServices::getInstance()->getContentLanguage();
-		if ( is_numeric( $namespace ) ) {
+		if (is_numeric($namespace)) {
 			return $namespace;
-		} elseif ( $language->getNsIndex( $namespace ) !== false ) {
-			return $language->getNsIndex( $namespace );
-		} elseif ( $namespace == '-' ) {
+		} elseif ($language->getNsIndex($namespace) !== false) {
+			return $language->getNsIndex($namespace);
+		} elseif ($namespace == '-') {
 			return NS_MAIN;
 		} else {
 			return -1;
 		}
 	}
 
-	private function getNsFragment() {
+	private function getNsFragment()
+	{
 		$this->namespace = (int)$this->namespace;
 		return $this->namespace > -1 ? "page_namespace = {$this->namespace}" : 'page_namespace != 8';
 	}
 
-	private function makeListItem( $row ) {
-		$title = Title::makeTitleSafe( $row->page_namespace, $row->page_title );
+	private function makeListItem($row)
+	{
+		$title = Title::makeTitleSafe($row->page_namespace, $row->page_title);
 		$linkRenderer = $this->getLinkRenderer();
-		if ( $title !== null ) {
+		if ($title !== null) {
 			$link = $row->page_is_redirect
-					? '<span class="allpagesredirect">' . $linkRenderer->makeKnownLink( $title ) . '</span>'
-					: $linkRenderer->makeKnownLink( $title );
-			return "<li>{$link}</li>\n";
+				? '<span class="allpagesredirect">' . $linkRenderer->makeKnownLink($title) . '</span>'
+				: $linkRenderer->makeKnownLink($title);
+			return "<div>
+						<h3>{$link}</h3>
+					</div>\n";
 		} else {
-			return "<!-- Invalid title " . htmlspecialchars( $row->page_title ) .
-				" in namespace " . htmlspecialchars( $row->page_namespace ) . " -->\n";
+			return "<!-- Invalid title " . htmlspecialchars($row->page_title) .
+				" in namespace " . htmlspecialchars($row->page_namespace) . " -->\n";
 		}
 	}
 
-	private function makeLimitLinks() {
+	private function makeLimitLinks()
+	{
 		$lang = $this->getLanguage();
-		$limits = [ 10, 20, 30, 50, 100, 150 ];
+		$limits = [10, 20, 30, 50, 100, 150];
 		$links = [];
-		foreach ( $limits as $limit ) {
-			if ( $limit != $this->limit ) {
-				$links[] = $this->makeSelfLink( $lang->formatNum( $limit ), 'limit', $limit );
+		foreach ($limits as $limit) {
+			if ($limit != $this->limit) {
+				$links[] = $this->makeSelfLink($lang->formatNum($limit), 'limit', $limit);
 			} else {
 				$links[] = (string)$limit;
 			}
 		}
-		return $this->msg( 'newestpages-limitlinks' )->rawParams(
-				$lang->pipeList( $links ) )->escaped();
+		return $this->msg('newestpages-limitlinks')->rawParams(
+			$lang->pipeList($links)
+		)->escaped();
 	}
 
-	private function makeRedirectToggle() {
+	private function makeRedirectToggle()
+	{
 		$label = $this->msg(
-				$this->redirects ? 'newestpages-hideredir' : 'newestpages-showredir' )->text();
-		return $this->makeSelfLink( $label, 'redirects', (int)!$this->redirects );
+			$this->redirects ? 'newestpages-hideredir' : 'newestpages-showredir'
+		)->text();
+		return $this->makeSelfLink($label, 'redirects', (int)!$this->redirects);
 	}
 
-	private function makeSelfLink( $label, $oname = false, $oval = false ) {
+	private function makeSelfLink($label, $oname = false, $oval = false)
+	{
 		$linkRenderer = $this->getLinkRenderer();
 		$self = $this->getPageTitle();
 		$attr = [];
 		$attr['limit'] = $this->limit;
 		$attr['namespace'] = $this->namespace;
 
-		if ( !$this->redirects ) {
+		if (!$this->redirects) {
 			$attr['redirects'] = 0;
 		}
 
-		if ( $oname ) {
+		if ($oname) {
 			$attr[$oname] = $oval;
 		}
 
-		return $linkRenderer->makeKnownLink( $self, $label, [], $attr );
+		return $linkRenderer->makeKnownLink($self, $label, [], $attr);
 	}
 
-	private function makeNamespaceForm() {
+	private function makeNamespaceForm()
+	{
 		$self = $this->getPageTitle();
 		$formDescriptor = [
 			'namespace' => [
 				'type' => 'namespaceselect',
 				'name' => 'namespace',
 				'id' => 'namespace',
-				'label' => $this->msg( 'newestpages-namespace' )->text(),
+				'label' => $this->msg('newestpages-namespace')->text(),
 				'all' => 'all',
 				'value' => $this->namespace,
 			]
 		];
 
-		$htmlForm = HTMLForm::factory( 'ooui', $formDescriptor, $this->getContext() );
+		$htmlForm = HTMLForm::factory('ooui', $formDescriptor, $this->getContext());
 		$htmlForm
-			->addHiddenField( 'limit', $this->limit )
-			->addHiddenField( 'redirects', $this->redirects )
-			->setMethod( 'get' )
-			->setSubmitText( $this->msg( 'newestpages-submit' )->text() )
-			->setWrapperLegend( null )
+			->addHiddenField('limit', $this->limit)
+			->addHiddenField('redirects', $this->redirects)
+			->setMethod('get')
+			->setSubmitText($this->msg('newestpages-submit')->text())
+			->setWrapperLegend(null)
 			->prepareForm()
-			->displayForm( false );
+			->displayForm(false);
 
-		 return true;
+		return true;
 	}
 
 	/**
 	 * @inheritDoc
 	 */
-	protected function getGroupName() {
+	protected function getGroupName()
+	{
 		return 'changes';
 	}
 }
